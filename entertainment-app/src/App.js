b89422c003a4a1664f5a6e32acc828c697b1745c
dhruvgosain14 <93120205+dhruvgosain14@users.noreply.github.com>
2022-06-13 14:52:39 +0530

    added swipr.js

--- entertainment-app/src/App.js
+++ entertainment-app/src/App.js
@@ -1,9 +1,11 @@
 import Search from './components/SearchComponent/Search'
+import Trending from './components/TrendingComponent/Trending'
 
 function App() {
   return (
     <div className="App">
       <Search/>
+      <Trending/>
     </div>
   );
 }
---
+++ entertainment-app/src/components/TrendingComponent/Trending.js
@@ -0,0 +1,23 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+    </Swiper>
+  );
+};
